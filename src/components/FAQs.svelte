8f2da01e85e67e42862cf2e2d23cfb1aa518814c
ZohaibAliJerov <zuhaib911alijerov@gmail.com>
2024-07-19 20:54:38 +0500

    made the landing page

---
+++ src/components/FAQs.svelte
@@ -0,0 +1,67 @@
+<script>
+  import SectionWrapper from "./SectionWrapper.svelte";
+
+  let faqs = [
+    {
+      question: "What is Svelte?",
+      answer:
+        "Svelte is a modern JavaScript framework that compiles your application into efficient, vanilla JavaScript at build time. It shifts much of the work to compile-time, resulting in highly optimized code and better performance.",
+    },
+    {
+      question:
+        "How is Svelte different from other frameworks like React or Vue?",
+      answer:
+        "Svelte differs fundamentally in its approach to building web applications. Unlike React or Vue, which rely on runtime libraries to manage components, Svelte compiles components into optimized JavaScript at build time. This approach reduces the amount of code shipped to the client and results in faster rendering and smaller bundle sizes.",
+    },
+    {
+      question: "Is Svelte suitable for large-scale applications?",
+      answer:
+        "Yes, Svelte is suitable for building large-scale applications. Its compile-time approach ensures that applications remain efficient and performant even as they grow in size. Many companies have successfully adopted Svelte for their complex web projects.",
+    },
+    {
+      question: "How does Svelte handle state management?",
+      answer:
+        "Svelte has a built-in reactive system that makes managing state straightforward. It uses reactive declarations (`$:`) to automatically update the DOM when state changes. Additionally, Svelte provides stores and context API for more advanced state management needs.",
+    },
+    {
+      question: "Is it easy to learn Svelte?",
+      answer:
+        "Yes, many developers find Svelte easy to learn due to its simple and intuitive syntax. It has a smaller API surface compared to other frameworks, which makes it quicker to pick up. Svelte's documentation and community resources also contribute to its ease of adoption.",
+    },
+  ];
+</script>
+
+<SectionWrapper id="faqs">
+  <div
+    class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center"
+  >
+    <div class="flex flex-col gap-2">
+      <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
+        For all your questions, qualms & queries
+      </p>
+      <h3
+        class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center"
+      >
+        Frequently Asked Questions
+      </h3>
+    </div>
+
+    <div class="flexx flex-col gap-8 sm:gap-10 md:gap-14 w-full">
+      {#each faqs as faq}
+        <div
+          class="flex flex-col gap-2 text-left max-w-[800px] w-full mx-auto relative p-4 px-6"
+        >
+          <div
+            class="absolute top-0 left-0 w-1/3 h-[1px] bg-slate-950 -translate-x-4"
+          ></div>
+          <div
+            class="absolute top-0 left-0 h-2/3 w-[1px] bg-slate-950 -translate-y-4"
+          ></div>
+
+          <h4 class="text-lg sm:text-xl md:text-2xl pt-2">{faq.question}</h4>
+          <p class="pl-2">{faq.answer}</p>
+        </div>
+      {/each}
+    </div>
+  </div>
+</SectionWrapper>
---
+++ src/components/Reviews.svelte
@@ -0,0 +1,91 @@
+<script>
+  import ReviewCard from "./ReviewCard.svelte";
+  import Reviews from "./Reviews.svelte";
+  import SectionWrapper from "./SectionWrapper.svelte";
+  import Stars from "./Stars.svelte";
+
+  let lim = true;
+  let reviewsList = [
+    {
+      name: "Zohaib Ali",
+      review:
+        "sowley molley is more than just a fitness app, its a lifestyle change. it helped me to develop healthy habits and make better choices about my diet and habits. i am incredibly thanful for this app and the positive impact it had on my life",
+      features: [
+        "Focus on healthy habits",
+        "Nutritional guidence",
+        "Positive lifestyle impact",
+      ],
+    },
+    {
+      name: "Zohaib Ali",
+      review:
+        "sowley molley is more than just a fitness app, its a lifestyle change. it helped me to develop healthy habits and make better choices about my diet and habits. i am incredibly thanful for this app and the positive impact it had on my life",
+      features: [
+        "Focus on healthy habits",
+        "Nutritional guidence",
+        "Positive lifestyle impact",
+      ],
+    },
+    {
+      name: "Zohaib Ali",
+      review:
+        "sowley molley is more than just a fitness app, its a lifestyle change. it helped me to develop healthy habits and make better choices about my diet and habits. i am incredibly thanful for this app and the positive impact it had on my life",
+      features: [
+        "Focus on healthy habits",
+        "Nutritional guidence",
+        "Positive lifestyle impact",
+      ],
+    },
+    {
+      name: "Zohaib Ali",
+      review:
+        "sowley molley is more than just a fitness app, its a lifestyle change. it helped me to develop healthy habits and make better choices about my diet and habits. i am incredibly thanful for this app and the positive impact it had on my life",
+      features: [
+        "Focus on healthy habits",
+        "Nutritional guidence",
+        "Positive lifestyle impact",
+      ],
+    },
+  ];
+</script>
+
+<SectionWrapper id="reviews">
+  <div
+    class="flex flex-col gap-10 sm:gap-14 md:gap-24 py-20 flex-1 items-center justify-center"
+  >
+    <div class="flex flex-col gap-2">
+      <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
+        Don't just take our word for it
+      </p>
+      <h3
+        class="text-4xl sm:text-5xl md:text-6xl max-w-[1000px] mx-auto w-full font-semibold text-center"
+      >
+        See what
+        <span class="text-indigo-400">Others</span> Have To Say
+      </h3>
+    </div>
+
+    <div class="flex flex-col md:flex-row gap-8 w-fit mx-auto">
+      <div class="flex flex-col gap-8">
+        {#each lim ? reviewsList.slice(0, 4) : reviewsList as reviewItem, index}
+          <ReviewCard {reviewItem} left={true} {index} />
+        {/each}
+      </div>
+      <div class="w-[1px] bg-slate-950 hidden md:flex"></div>
+      <div class="flex flex-col gap-8">
+        {#each lim ? reviewsList.slice(0, 4) : reviewsList as reviewItem, index}
+          <ReviewCard {reviewItem} {index} />
+        {/each}
+      </div>
+    </div>
+    <button on:click={() => (lim = !lim)} class="specialBtn"
+      ><p>{lim ? "Show More" : "Show Less"}</p></button
+    >
+    <div class="flex flex-col gap-10">
+      <p class="mx-auto text-lg sm:text-xl md:text-2xl font-semibold">
+        Trusted by 1,296 Soldiers
+      </p>
+      <Stars />
+    </div>
+  </div>
+</SectionWrapper>
